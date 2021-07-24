9eb48aa8129d34be696aeb99496fff6c19ba5e55
Manskrrrr <henrique.manske.tierling@gmail.com>
2021-07-24 16:25:49 -0300

    aula2

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from os import name
+import re
 from flask import Flask, render_template, redirect, request
-from repositorio import insert, conexao
+from repositorio import insert, conexao, select, select_id, delete, update
 
 app = Flask(__name__)
 
@@ -21,7 +22,46 @@ def salvar():
     fone = request.form['phone']
     db = conexao()
     insert(db, nome, sobrenome, cpf, email, fone)
+    return redirect('/listar/todos')
 
-    return redirect('/cadastro')
+@app.route('/listar/todos', methods=['get'])
+def listar():
+    db = conexao()
+    lista_banco = select(db)
+    return render_template('listar.html',lista_banco = lista_banco)
+
+@app.route('/listar/<int:id>/', methods=['get'])
+def listar_por_id(id):
+    db = conexao()
+    registro = select_id(db, id)
+    return render_template('listar_id.html',lista_banco = registro)
+
+@app.route('/delete', methods=['get'])
+def deletar():
+    id = request.args['id']
+    db = conexao()
+    delete(db, id)
+    return redirect('/listar/todos')
+
+@app.route('/alterar', methods=['get'])
+def alterar():
+    id = request.args['id']
+    db = conexao()
+    registro = select_id(db, id)
+    return render_template('listar_id.html', id = registro[0], nome=registro[1],
+    sobrenome = registro[2], cpf = registro[3], email = registro[4],
+    telefone = registro[5])
+
+@app.route('/alterar/salvar', methods=['post'])
+def alterar_salvar():
+    id = request.form['id']
+    nome = request.form['name']
+    sobrenome = request.form['last_name']
+    cpf = request.form['cpf']
+    email = request.form['email']
+    telefone = request.form['phone']
+    db = conexao()
+    update(db, id, nome, sobrenome, cpf, email, telefone)
+    return redirect('/listar/todos')
 
 app.run(debug=True)
--- repositorio.py
+++ repositorio.py
@@ -10,6 +10,32 @@ def conexao():
     )
     return db
 
+def select(db):
+    comando_sql = 'SELECT * FROM CONTATOS'
+    try:
+        cursor = db.cursor()
+        cursor.execute(comando_sql)
+        resultado = cursor.fetchall()
+        #for registro in resultado:
+            #print(f'id: {registro[0]}, nome: {registro[1]}, sobrenome: {registro[2]}',
+            #f'cpf: {registro[3]}, email: {registro[4]}, telefone: {registro[5]}')
+        
+        cursor.close()
+        return resultado
+    except Exception as error:
+        print(error)
+
+def select_id(db, id_registro):
+    comando_sql = f'SELECT * FROM CONTATOS WHERE id = {id_registro}'
+    try:
+        cursor = db.cursor()
+        cursor.execute(comando_sql)
+        resultado = cursor.fetchone()
+        cursor.close()
+        return resultado
+    except Exception as error:
+        print(error)
+
 def insert(db, nome, sobrenome, cpf, email, fone):
     comando_sql = 'INSERT INTO CONTATOS (NOME, SOBRENOME, CPF, EMAIL, TELEFONE) VALUES (%s, %s, %s, %s, %s)'
     parametros = (nome, sobrenome, cpf, email, fone)
@@ -21,4 +47,24 @@ def insert(db, nome, sobrenome, cpf, email, fone):
         print('Registro inserido com sucesso.')
     except Exception as error:
         print(error)
-        
+
+def delete(db, id_registro):
+    comando_sql = f'DELETE FROM CONTATOS WHERE ID = {id_registro}'
+    try:
+        cursor = db.cursor()
+        cursor.execute(comando_sql)
+        db.commit()
+        cursor.close()
+    except Exception as error:
+        print(error)
+
+def update(db, id, nome, sobrenome, cpf, email, telefone):
+    comando_sql = "UPDATE CONTATOS SET NOME = %s, SOBRENOME = %s, CPF = %s, EMAIL = %s, TELEFONE = %s WHERE ID = %s"
+    parametros = (nome, sobrenome, cpf, email, telefone, id)
+    try:
+        cursor = db.cursor()
+        cursor.execute(comando_sql, parametros)
+        db.commit()
+        cursor.close()
+    except Exception as error:
+        print(error)
